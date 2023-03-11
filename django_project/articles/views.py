8e65692868caba43ab18ee40ad5188fc678c2f70
gunjanak <janak.klal@gmail.com>
2023-03-11 15:50:29 +0545

    login reuired to create, update and delete

--- django_project/articles/views.py
+++ django_project/articles/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin 
+
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (
     UpdateView,
@@ -17,7 +19,7 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
     model = Article
     fields = {
         "title",
@@ -25,18 +27,21 @@ class ArticleUpdateView(UpdateView):
     }
     template_name = "article_edit.html"
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
     template_name = "article_new.html"
     fields = (
         "title",
         "body",
-        "author",
     )
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
