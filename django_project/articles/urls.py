e984f1c1279ce28e2f8c7b21d5f360ce1cc7ecfb
gunjanak <janak.klal@gmail.com>
2023-03-11 12:22:38 +0545

    create done

--- django_project/articles/urls.py
+++ django_project/articles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
 from .views import (
     ArticleListView,
     ArticleDetailView,
     ArticleUpdateView,
-    ArticleDeleteView)
+    ArticleDeleteView,
+    ArticleCreateView,
+)
+
 
 
 urlpatterns = [
@@ -14,6 +16,8 @@ urlpatterns = [
          name="article_edit"),
     path("<int:pk>/delete/",ArticleDeleteView.as_view(),
          name="article_delete"),
+    path("new/",ArticleCreateView.as_view(),
+         name="article_new"),
     path("",ArticleListView.as_view(),
          name="article_list"),
 ]
