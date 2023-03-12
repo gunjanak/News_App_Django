9ea215e0c957f96b800d6252ac224dad291a3d54
gunjanak <janak.klal@gmail.com>
2023-03-12 14:51:18 +0545

    two tests added

--- django_project/articles/tests.py
+++ django_project/articles/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class ArticleTests(TestCase):
+    def test_url_exists_at_correct_location_articles(self):
+        response = self.client.get("/articles/")
+        self.assertEqual(response.status_code,200)
+
+    def test_article_list(self):
+        response = self.client.get(reverse("article_list"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"article_list.html")
+
+
+   
+    
